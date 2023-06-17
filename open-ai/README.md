262bc8a89c6f55f0fd81d429f0d2359577c585ec
Aleksandar Ristic <storiaca@gmail.com>
2023-06-17 12:49:25 +0200

    Add links to readme

--- open-ai/README.md
+++ open-ai/README.md
@@ -10,3 +10,11 @@ https://www.youtube.com/watch?v=C4ve8Kjw9ZY&t=23s&ab_channel=TheNetNinja
 ## Link to github
 
 https://github.com/iamshaunjp/openAI-basics
+
+## API Link
+
+https://platform.openai.com/docs/api-reference
+
+## Open AI Documentation
+
+https://platform.openai.com/docs/introduction
--- open-ai/app.js
+++ open-ai/app.js
@@ -1,8 +1,8 @@
-const readline = require('readline')
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 
-rl.question("YouTube Video Title: \n", (title) => console.log(title))
+rl.question("YouTube Video Title: \n", (title) => console.log(title));
